35666f77479c562c9ca1eb851d6fdb5d97d5f359
Lucas Peres <lucasperesss@gmail.com>
2021-11-24 16:33:41 -0300

    Corrige erros de lint

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 import App from './App';
 
--- src/reducer/player.js
+++ src/reducer/player.js
@@ -1,4 +1,4 @@
-import { SET_USER_DATA } from "../actions";
+import { SET_USER_DATA } from '../actions';
 
 const INITIAL_STATE = {
   name: '',
@@ -7,12 +7,12 @@ const INITIAL_STATE = {
 
 export default function playerReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
-    case SET_USER_DATA:
-      return {
-        ...state,
-        name: action.payload.name,
-        email: action.payload.email,
-      }
-    default: return state;
+  case SET_USER_DATA:
+    return {
+      ...state,
+      name: action.payload.name,
+      email: action.payload.email,
+    };
+  default: return state;
   }
 }
