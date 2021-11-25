986556ab2505466c57d3398b95f333cd14b75e3b
Antônio Campos <aclcampos@live.com>
2021-11-25 17:56:30 -0300

    Adicionado função shuffleArray para embaralhar as opções de resposta

--- src/pages/Trivia.js
+++ src/pages/Trivia.js
@@ -8,6 +8,7 @@ class Trivia extends Component {
     super();
 
     this.getAnswers = this.getAnswers.bind(this);
+    this.shuffleArray = this.shuffleArray.bind(this);
   }
 
   getAnswers() {
@@ -32,7 +33,20 @@ class Trivia extends Component {
         {questions[0].correct_answer}
 
       </button>];
-    return allAnswers;
+    return this.shuffleArray(allAnswers);
+  }
+
+  // a função shuffleArray utilizamos do link: https://www.horadecodar.com.br/2021/05/10/como-embaralhar-um-array-em-javascript-shuffle/
+  shuffleArray(arr) {
+    // Loop em todos os elementos
+    for (let i = arr.length - 1; i > 0; i -= 1) {
+      // Escolhendo elemento aleatório
+      const j = Math.floor(Math.random() * (i + 1));
+      // Reposicionando elemento
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    // Retornando array com aleatoriedade
+    return arr;
   }
 
   render() {
