c6e9b2a0fb852c11036d045212ca8303588b6dd1
Lucas Peres <lucasperesss@gmail.com>
2021-11-29 17:46:27 -0300

    Req 13 ok

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,15 +1,39 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  feedbackMessageGenerator() {
+    const { numberOfSuccess } = this.props;
+    const MIN = 3;
+    if (numberOfSuccess < MIN) return 'Podia ser melhor...';
+    return 'Mandou bem!';
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <p data-testid="feedback-text"> pagina de feedback </p>
+        <p data-testid="feedback-text">
+          {' '}
+          {
+            this.feedbackMessageGenerator()
+          }
+          {' '}
+
+        </p>
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  numberOfSuccess: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  numberOfSuccess: state.playerReducer.answeredCorrectly,
+});
+
+export default connect(mapStateToProps)(Feedback);
