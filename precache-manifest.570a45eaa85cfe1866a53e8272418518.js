db2f2f0a81fa4d7fa09dcb38a8930cfc00447966
Antônio <aclcampos@live.com>
2022-01-09 02:18:26 -0300

    Updates

---
+++ precache-manifest.570a45eaa85cfe1866a53e8272418518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c18bcc264bfb72aad80674a48aa1d8",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "e5604c654ced110a48fc",
+    "url": "/trivia-react-redux/static/css/main.4ed0fc39.chunk.css"
+  },
+  {
+    "revision": "c61c887eee77c733cbe6",
+    "url": "/trivia-react-redux/static/js/2.7e32b3e1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/trivia-react-redux/static/js/2.7e32b3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5604c654ced110a48fc",
+    "url": "/trivia-react-redux/static/js/main.63376865.chunk.js"
+  },
+  {
+    "revision": "cab236066c8a027c2fd3",
+    "url": "/trivia-react-redux/static/js/runtime-main.b83dab60.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-react-redux/static/media/trivia.466d153e.png"
+  }
+]);
